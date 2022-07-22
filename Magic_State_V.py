e8a3b4d8f5da5ffb83665fb30d521f7c9c077343
andrewprojansky <andrewprojansky@gmail.com>
2022-07-22 14:19:00 -0400

    Update Magic_State_V.py

--- Magic_State_V.py
+++ Magic_State_V.py
@@ -25,9 +25,23 @@ CNot = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
 Defines all useful states and matrix products for simulation
 """
 
-univ_rot = np.matmul(T, np.matmul(H, np.matmul(T, H)))
+#univ_rot = np.matmul(T, np.matmul(H, np.matmul(T, H)))
 magic_state = np.array([1/np.sqrt(2),1/np.sqrt(2)*np.exp(1j*np.pi/4)])
+rho_m = np.tensordot(magic_state, np.conjugate(magic_state),0)
 gate_dict = {1: T, 2: H}
+#gate = {1:H}
+
+def cc(gate):
+    """
+    Returns complex conjugate of gate
+
+    Parameters
+    ----------
+    gate: list
+        Unitary gate that can be applied on a state
+    """
+
+    return np.transpose(np.matrix.conjugate(gate))
 
 class Experiment:
     """
@@ -51,7 +65,7 @@ class Experiment:
         self,
         num_steps: int = 1,
         magic: bool = False,
-        state: list = np.array([1,0])
+        state: list = np.array([[1,0],[0,0]])
 
     ):
         self.num_steps = num_steps
@@ -59,7 +73,7 @@ class Experiment:
         self.state = state
 
 
-        self.angles = []
+        self.angles = [[],[],[]]
 
     ################# run functions ##################################
     def run_stepwise(self):
@@ -69,28 +83,14 @@ class Experiment:
             if (gate_choice == 1) and (self.magic):
                 self.magic_step()
             else:
-                self.state = np.matmul(gate, self.state)
-                self.angles.append(self.__get_angle(self.state))
+                self.state = np.matmul(gate, np.matmul(self.state, cc(gate)))
+                self.__gen_xyz_points()
 
     def magic_steps(self):
         pass
 
-    def __get_angle(self, state):
-        """
-        get an angle for a single state
-        """
-        first_mag = np.sqrt(np.real(state[0]) ** 2 + np.imag(state[0]) ** 2)
-        second_mag = np.sqrt(np.real(state[1]) ** 2 + np.imag(state[1]) ** 2)
-        first_phi = np.arctan2(np.imag(state[0]), np.real(state[0]))
-        second_phi = np.arctan2(np.imag(state[1]), np.real(state[1]))
-        phi = second_phi - first_phi
-        theta = 2 * np.arccos(np.real(state[0] * np.exp(-1j * first_phi)))
-
-        return [theta, phi]
-
     ##############plot functions ########################
     def plot(self):
-        states_xyz = [self.__gen_xyz_points(self.angles)]
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
         u, v = np.mgrid[0 : 2 * np.pi : 50j, 0 : np.pi : 50j]
@@ -98,11 +98,10 @@ class Experiment:
         ys = np.sin(u) * np.sin(v)
         zs = np.cos(v)
         ax.plot_surface(xs, ys, zs, color="lightgrey", alpha=0.3)
-        for x in states_xyz:
-            ax.scatter(x[0], x[1], x[2], marker="o", alpha=0.9)
+        ax.scatter(self.angles[0], self.angles[1], self.angles[2], marker="o", alpha=0.9)
         plt.show()
 
-    def __gen_xyz_points(self, angle_arr):
+    def __gen_xyz_points(self):
         """
         Parameters
         ----------
@@ -110,25 +109,9 @@ class Experiment:
             list of the form [[theta1,phi1],[theta2,phi2] ...].
 
         """
-        x = np.array([])
-        y = np.array([])
-        z = np.array([])
-        for i in angle_arr:
-            x = np.append(x, self.__get_x(i[0], i[1]))
-            y = np.append(y, self.__get_y(i[0], i[1]))
-            z = np.append(z, self.__get_z(i[0], i[1]))
-
-        return x, y, z
-
-    def __get_x(self, theta, phi):
-        return np.cos(phi) * np.sin(theta)
-
-    def __get_y(self, theta, phi):
-        return np.sin(phi) * np.sin(theta)
-
-    def __get_z(self, theta, phi):
-        return np.cos(theta)
-
+        self.angles[0].append(2*np.real(self.state[1][0]))
+        self.angles[1].append(2*np.imag(self.state[1][0]))
+        self.angles[2].append(2*self.state[0][0]-1)
 
 #%%
 exp = Experiment(num_steps=1000)
